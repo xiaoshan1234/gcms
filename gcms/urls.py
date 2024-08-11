87aedfe3f81fc2f012377f03a4601b50b587f4e9
loner <liuhao2495@outlook.com>
2024-08-11 17:39:47 +0800

    app: index detail: url

--- gcms/urls.py
+++ gcms/urls.py
@@ -13,9 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-
+from django.urls import include,re_path
+import app_index.views as idx_vw
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    re_path(r'^index/?', idx_vw.index),   
+    re_path('', idx_vw.index)
 ]
