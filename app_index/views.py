64c472a29389683e90134d015f95452bf3290c28
loner <liuhao2495@outlook.com>
2024-08-11 17:39:36 +0800

    app: index detail: view

--- app_index/views.py
+++ app_index/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import redirect, render
 
 # Create your views here.
+def index(req:HttpRequest):
+    context = {}
+    return render(req,"app_index/index.html",context)
